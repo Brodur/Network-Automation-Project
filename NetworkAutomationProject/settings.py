08dfacbfbce48319fbd48a77e8312f0608adc89a
Ranaveer_Dhanoa <singhranaveer00070@gmail.com>
2020-12-07 13:23:22 -0600

    Merge remote-tracking branch 'origin/views-templates' into Ranaveer

--- NetworkAutomationProject/settings.py
+++ NetworkAutomationProject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'NetworkAutomationProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
