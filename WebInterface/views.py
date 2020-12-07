755a4a1c12053db109eada82106214926910be43
Ranaveer_Dhanoa <singhranaveer00070@gmail.com>
2020-12-07 14:27:45 -0600

    views.pyMergeConflictSolved

--- WebInterface/views.py
+++ WebInterface/views.py
@@ -4,23 +4,22 @@ import requests
 import sys
 import json
 from Hostname import *
-from GetBanner import *
 
 # Create your views here.
 
-def index(request):
-  context = {
-<<<<<<< HEAD
-    'sample_var': 'Hostname.getHostname'
-  }
+# def index(request):
+#   context = {
+#     'sample_var': 'Hostname.getHostname',
+#     'sample_var': 'Hello, my name is Jeff',
+#     'bannerMotd': 'Unauthorized access is prohibited!',
+#     'hostname': 'R1-BLDG2'
+#   }
 
-  return render(request, 'index.html', context)
+#   return render(request, 'index.html', context)
+hostname1 = Hostname('10.10.20.48','developer','C1sco12345','CSRV-1')
+abcd = hostname1.getHostname()
 
-=======
-    'sample_var': 'Hello, my name is Jeff',
-    'bannerMotd': 'Unauthorized access is prohibited!',
-    'hostname': 'R1-BLDG2'
-  }
 
-  return render(request, 'index.html', context)
->>>>>>> origin/master
+# returnedHostname = hostname1.getHostname()
+# hostname1 = hostname1.setHostname()
+# print(returnedHostname)
