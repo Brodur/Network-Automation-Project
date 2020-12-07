6edffeafe408919642d9655e5c3312e93c664ca5
Ranaveer_Dhanoa <singhranaveer00070@gmail.com>
2020-12-07 16:48:43 -0600

    added routes for the form

--- WebInterface/urls.py
+++ WebInterface/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('config/set-hostname', views.set_hostname, name="set_hostname"),
+    path('config/set-banner', views.set_banner, name="set_banner")
 ]
