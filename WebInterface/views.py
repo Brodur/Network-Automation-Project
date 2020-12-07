ca5da39f4c93bcefde8775761dfb8d9072cde517
Ranaveer_Dhanoa <singhranaveer00070@gmail.com>
2020-12-07 14:56:08 -0600

    ImportFromDirectory

--- WebInterface/views.py
+++ WebInterface/views.py
@@ -3,24 +3,18 @@ from django.http import HttpResponse
 import requests
 import sys
 import json
-from Hostname import *
-from GetBanner import *
+from .Classes.Hostname import *
 
 # Create your views here.
 
 def index(request):
   context = {
-<<<<<<< HEAD
+
     'sample_var': 'Hostname.getHostname'
-  }
-
-  return render(request, 'index.html', context)
-
-=======
     'sample_var': 'Hello, my name is Jeff',
     'bannerMotd': 'Unauthorized access is prohibited!',
     'hostname': 'R1-BLDG2'
   }
 
   return render(request, 'index.html', context)
->>>>>>> origin/master
+  print(Hostname.getHostname())
