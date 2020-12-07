e8aab2f83296f13c97da71bdf7beb2acc42af359
Ranaveer_Dhanoa <singhranaveer00070@gmail.com>
2020-12-07 16:50:47 -0600

    completed get and set for the forms

--- WebInterface/views.py
+++ WebInterface/views.py
@@ -4,18 +4,35 @@ import requests
 import sys
 import json
 from .Classes.Hostname import *
+from .Classes.Banner import *
+from django.views.decorators.csrf import csrf_exempt
+
+
 
 # Create your views here.
+hostname1 = Hostname('10.10.20.48','developer','C1sco12345')
+banner = Banner('10.10.20.48','developer','C1sco12345')
 
-def index(request):
-  hostname1 = Hostname('10.10.20.48','developer','C1sco12345')
+def index(request):   
   hostname = hostname1.getHostname()
-#   hostname = json.loads(hostname)
-  hostname = hostname['Cisco-IOS-XE-native:hostname']
+  hostname = hostname['Cisco-IOS-XE-native:hostname']   
+  returnedBanner = banner.getBanner()
+  returnedBanner =returnedBanner['Cisco-IOS-XE-native:banner']  
+
+
   context = {
-    'bannerMotd': 'afsdfsdfsaseawe1 ibited!',
+    'bannerMotd': returnedBanner,
     'hostname': hostname
   }
 
   return render(request, 'index.html', context)
-#   print(Hostname.getHostname())
+
+
+@csrf_exempt
+def set_hostname(request):
+#     host
+    newHostname = hostname1.setHostname(request.POST.get('hostname-hostname'))
+    return HttpResponse(newHostname)
+@csrf_exempt
+def set_banner(request):
+    return HttpResponse('banner banner banner')
