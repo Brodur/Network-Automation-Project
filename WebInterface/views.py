8db67f39e7fff0b2178531eebc9ceef22d3f5f03
Ranaveer_Dhanoa <singhranaveer00070@gmail.com>
2020-12-07 12:59:54 -0600

    methodCallFromSeparateFile

--- WebInterface/views.py
+++ WebInterface/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import requests
+import sys
+import json
+from Hostname import *
+from GetBanner import *
 
 # Create your views here.
 
-def index(request):
-  return HttpResponse("Hello World,")
+# def index(request):
+#   return HttpResponse("Hello World,")
+
+Hostname.getHostname
+
+  
