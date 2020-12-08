3b458cdd1b0e4b66753c4372b75bc56ab69f258c
Ranaveer_Dhanoa <singhranaveer00070@gmail.com>
2020-12-08 15:23:46 -0600

    added the remaining getters but commented out

--- WebInterface/views.py
+++ WebInterface/views.py
@@ -16,13 +16,27 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 hostname1 = Hostname('10.10.20.48','developer','C1sco12345')
 banner = Banner('10.10.20.48','developer','C1sco12345')
+# enPassword = EnablePassword('10.10.20.48','developer','C1sco12345')
+# execTimout = ExecTimeout('10.10.20.48','developer','C1sco12345')
+# intAddress = InterfaceAddress('10.10.20.48','developer','C1sco12345')
+# intDescription = InterfaceDescription('10.10.20.48','developer','C1sco12345')
+
 
 
 def index(request):   
   hostname = hostname1.getHostname()
   hostname = hostname['Cisco-IOS-XE-native:hostname']   
   returnedBanner = banner.getBanner()
-  returnedBanner =returnedBanner['Cisco-IOS-XE-native:banner']  
+  returnedBanner = returnedBanner['Cisco-IOS-XE-native:banner'] 
+  # password = enPassword.getEnablePassword() 
+  # password = password['Cisco-IOS-XE-native:']
+  # timeout = execTimeout.getExecTimeout()
+  # timeout = timeout['Cisco-IOS-XE-native:']
+  # ipAddress = intAddress.getInterfaceAddress()
+  # ipAddress = ipAddress['Cisco-IOS-XE-native:']
+  # interfaceDesc = intDescription.getInterfaceDescription()
+  # interfaceDesc = interfaceDesc['Cisco-IOS-XE-native:']
+
 
 
   context = {
