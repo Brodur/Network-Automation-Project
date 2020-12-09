a392dc4fbe3936c6bd3edde725c1dc2a28425e8a
nhfoy <57771543+nhfoy@users.noreply.github.com>
2020-12-09 10:55:21 -0600

    Adding Updated Enable Password
    Fixed the issue when no password is set and the method is called

--- WebInterface/Classes/EnablePassword.py
+++ WebInterface/Classes/EnablePassword.py
@@ -13,7 +13,6 @@ class EnablePassword:
     def getEnablePassword(self):
         url = "https://{h}/restconf/data/Cisco-IOS-XE-native:native/enable/password/secret".format(h=self.hostAddress)
 
-
         # These headers receive the data in json format
         headers = {'Content-Type': 'application/yang-data+json',
                    'Accept': 'application/yang-data+json'}
@@ -21,25 +20,27 @@ class EnablePassword:
         # this statement performs a GET on the specified url
         response = requests.get(url, auth=(self.username, self.password),
                                 headers=headers, verify=False)
+        if (response):
+            return(json.loads(response.text)) 
+        else:
+            return('Enable Password Is Not Set')
 
-        return(json.loads(response.text)) 
-
-    def setEnablePassworde(self, enablePassword):
+    def setEnablePassword(self, enablePassword):
         # url string to issue GET request
         url = "https://{h}/restconf/data/Cisco-IOS-XE-native:native/enable/password/secret".format(h=self.hostAddress)
-
         # THIS Line will need to be altered for the website, but essentially, we just want user input
         payload = "{\"secret\": \"" + enablePassword + "\"}"
-
-
         # These headers reecive the data in json format
         headers = {'Content-Type': 'application/yang-data+json',
                    'Accept': 'application/yang-data+json'}
-
         # this statement performs a GET on the specified url
         response = requests.put(url, auth=(self.username, self.password),
                                 data=payload, headers=headers, verify=False)
-
         # print the json that is returned
         return(response.text)
 
+
+#test out get response, see whats returned,  if good, good. If bad, do soemthing
+passwordss = EnablePassword('10.10.20.48','developer','C1sco12345')
+returnedBanner = passwordss.getEnablePassword()
+print(returnedBanner)
