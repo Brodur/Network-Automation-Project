6da3ee6607b54d994ed7750651a4958aaed92f01
nhfoy <57771543+nhfoy@users.noreply.github.com>
2020-12-09 10:56:22 -0600

    Adding updated Enable Password file
    Fixed the issue when a password is not set and the GetEnablePassword method is called

--- Python HTTP Request Classes/Enable Password/EnablePassword.py
+++ Python HTTP Request Classes/Enable Password/EnablePassword.py
@@ -1,51 +1,46 @@
 import sys
 import requests
+import json
 
 requests.packages.urllib3.disable_warnings()
 
 class EnablePassword:
-    def __init__(self, hostAddress, username, password, enablePassword):
+    def __init__(self, hostAddress, username, password):
         self.hostAddress = hostAddress
         self.username = username
         self.password = password
-        self.enablePassword = enablePassword
     
     def getEnablePassword(self):
         url = "https://{h}/restconf/data/Cisco-IOS-XE-native:native/enable/password/secret".format(h=self.hostAddress)
 
-        # These headers reecive the data in json format
+        # These headers receive the data in json format
         headers = {'Content-Type': 'application/yang-data+json',
                    'Accept': 'application/yang-data+json'}
 
         # this statement performs a GET on the specified url
         response = requests.get(url, auth=(self.username, self.password),
                                 headers=headers, verify=False)
+        if (response):
+            return(json.loads(response.text)) 
+        else:
+            return('Enable Password Is Not Set')
 
-        # print the json that is returned
-        print(response.text)
-        return(response.text)   
-
-    def setEnablePassworde(self):
+    def setEnablePassword(self, enablePassword):
         # url string to issue GET request
         url = "https://{h}/restconf/data/Cisco-IOS-XE-native:native/enable/password/secret".format(h=self.hostAddress)
-
         # THIS Line will need to be altered for the website, but essentially, we just want user input
-        payload = "{\"secret\": \"" + self.enablePassword + "\"}"
-
+        payload = "{\"secret\": \"" + enablePassword + "\"}"
         # These headers reecive the data in json format
         headers = {'Content-Type': 'application/yang-data+json',
                    'Accept': 'application/yang-data+json'}
-
         # this statement performs a GET on the specified url
         response = requests.put(url, auth=(self.username, self.password),
                                 data=payload, headers=headers, verify=False)
-
         # print the json that is returned
         return(response.text)
 
-#Below is just testing out the methods above
-#hostname1 is just for python, website will need user input
-enable = EnablePassword('10.10.20.48','developer','C1sco12345','Cisco1234')
-returnedEnable = enable.getEnablePassword()
-enable = enable.setEnablePassworde()
-print(returnedEnable)
+
+#test out get response, see whats returned,  if good, good. If bad, do soemthing
+passwordss = EnablePassword('10.10.20.48','developer','C1sco12345')
+returnedBanner = passwordss.getEnablePassword()
+print(returnedBanner)
